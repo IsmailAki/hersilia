a8a1811a18aa88f51841dd205e2ba2ff2ee76ac4
Abdullah Aki <abdullahismailaki@gmail.com>
2021-11-06 03:29:30 +0300

    add router
    Signed-off-by: Abdullah Aki <abdullahismailaki@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+	http.ListenAndServe(":8080", mux)
 }
